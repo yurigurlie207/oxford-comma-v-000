33511121fa95034eb51a96e7b46301ebcb9c77a1
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 00:36:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.size = 1 then 
+  if array.size = 1 then
     array.to_s
   else
-  last = array.pop
-  array.join(",") + ", and #{last}"
-end
+    last = array.pop
+    array.join(",") + ", and #{last}"
+  end
 end
