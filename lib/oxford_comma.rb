2f720fd4186e10e49c8b18c6692b0aa823e8545e
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 00:42:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  if array.size == 1 then
+    array.pop
+  elsif array.size == 2 then
+    last = array.last
+    first = array.first
+    "#{first} and #{last}"
+  else
+    last = array.pop
+    array.join(", ") + ", and #{last}"
+  end
+
 end
